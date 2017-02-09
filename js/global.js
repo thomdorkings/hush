0b59c51ef1465ce9643e26521e7768eba9bbc8f3
Thom Dorkings <hello@thomdorkings.com>
2017-02-09 23:01:00 +0000

    Landing page and layout

--- js/global.js
+++ js/global.js
@@ -1,10 +1,36 @@
 $(function() {
 
+/*
 	var colourscheme = $('body').attr('id');
 	
 	if(!colourscheme) {} else {
 		$('<style>.underline:after { background: #'+colourscheme+'; } .cover, #share { background: #'+colourscheme+'; }</style>').appendTo('head');
 	}
+*/
+	
+	$('.slideshow1').slick({
+		autoplay: true,
+		arrows: false,
+		fade: true,
+		pauseOnHover: false,
+		swipe: false,
+		draggable: false,
+		touchMove: false,
+		speed: 1200,
+		autoplaySpeed: 3300
+	});
+	
+	$('.slideshow2').slick({
+		autoplay: true,
+		arrows: false,
+		fade: true,
+		pauseOnHover: false,
+		swipe: false,
+		draggable: false,
+		touchMove: false,
+		speed: 1500,
+		autoplaySpeed: 3000
+	});
 	
 	$('.share').on('click', function() {
 		$('#share').addClass('open');
